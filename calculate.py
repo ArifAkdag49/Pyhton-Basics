8fd7fe431639ad18d4b27f76739594073903351c
arifakdag <66373692+arifakdag@users.noreply.github.com>
2020-06-14 16:14:06 +0200

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,18 @@
+islem_liste=["+","-","*","/"]
+islem_liste=input("lütfen islem giriniz:..")
+sayi1=int(input("bir sayi girin:.."))
+sayi2=int(input("bir sayi girin:"))
+
+if  islem_liste == "+":
+       print("sonuc:" , sayi1+sayi2)
+
+elif islem_liste =="-":
+            print("sonuc:" ,sayi1-sayi2)
+elif islem_liste =="*":
+            print("sonuc:" ,sayi1*sayi2)
+    
+elif islem_liste =="/":
+            print("sonuc:", sayi1/sayi2)
+
+    
+
